a62e4d686567e53bcb14188960987294eac335a9
Reo Tamai <reotamai5@gmail.com>
2022-03-05 03:31:37 -0800

    delete db file before start

--- backend/Program.cs
+++ backend/Program.cs
@@ -2,6 +2,7 @@ using backend.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using backend.Models;
+using Microsoft.Data.Sqlite;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -59,11 +60,26 @@ app.UseAuthorization();
 
 app.MapControllers();
 
+var DatabasePath = "DailyStyle.db";
+FileInfo fi = new FileInfo(DatabasePath);
+try
+{
+    if (fi.Exists)
+    {
+        GC.Collect();
+        GC.WaitForPendingFinalizers();
+        fi.Delete();
+    }
+}
+catch(Exception ex)
+{
+    fi.Delete();
+}
+
 using (var scope = app.Services.CreateScope()) {
     var services = scope.ServiceProvider;
 
     var context = services.GetRequiredService<DailyStyleDBContext>();   
-
     context.Database.Migrate(); 
     
     Clothing c1 = new Clothing(){
