b7e49a57240845a1730565afff48ad7b8d3c1526
Edmond Wong <wongedmondk@gmail.com>
2022-03-19 15:26:44 -0700

    Changed EditCloth page to be based on AddClorth page; First iteration of trying to grab proper image information based on URL

--- frontend/src/pages/EditCloth.js
+++ frontend/src/pages/EditCloth.js
@@ -1,43 +1,187 @@
-import { withAuthenticationRequired } from "@auth0/auth0-react";
+import axios from "axios";
 import { useEffect, useState } from "react";
+import { useAuth0, withAuthenticationRequired } from "@auth0/auth0-react";
+import { toast } from "react-toastify";
+import { baseUrl } from "../lib/constant";
+import { useNavigate } from "react-router";
 import Home from "./Home";
-import Box from '@mui/material/Box';
-import {Button, Input, Container, FormControl, TextField, Stack } from "@mui/material";
+import {
+  Autocomplete,
+  Box,
+  Button,
+  Container,
+  Divider,
+  FormControl,
+  IconButton,
+  Input,
+} from "@mui/material";
+import TextField from "@mui/material/TextField";
+import CircularProgress from "@mui/material/CircularProgress";
 import PhotoCamera from "@mui/icons-material/PhotoCamera";
+import { useLocation } from 'react-router-dom';
 
 function EditCloth() {
+  const navigation = useNavigate();
+  axios.defaults.baseURL = baseUrl;
+
+  const { getAccessTokenSilently } = useAuth0();
+
+  const location = useLocation();
+  const id = location.pathname.split("/").pop();
+
+  const getClothings = async (e) => {
+    try {
+      const token = await getAccessTokenSilently();
+      axios.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+      const response = await axios.get(`/api/clothings/${id}`)
+      .then((response) => {
+        console.log(response);
+      })
+    } catch (error) {
+      toast.error(error.message);
+    }
+  }
+
+  useEffect(() => {
+    getClothings();
+  }, [""]);
+
+  const [base64, setBase64] = useState(null);
+  const postClothings = async (e) => {
+    e.preventDefault();
+    let imageType = "";
+    let image = "";
+    if (base64 !== null) {
+      imageType = base64.split(",")[0];
+      image = base64.split(",")[1];
+    }
+
+    try {
+      const token = await getAccessTokenSilently();
+      axios.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+      console.log(selectedTags.map((t) => t.id));
+      const response = await axios
+        .post("/api/clothings", {
+          Title: [document.getElementById("title").value],
+          Description: [document.getElementById("description").value],
+          Image: [image],
+          ImageType: [imageType],
+          Tags: selectedTags.map((t) => t.id),
+        })
+        .then((response) => {
+          navigation("/ListClothes");
+          toast.success("Clothing added successfully");
+        })
+        .catch((error) => {
+          toast.error(error.message);
+        });
+    } catch (error) {
+      toast.error(error.message);
+    }
+  };
+
+  const [fileState, setFileImage] = useState(null);
+
+  useEffect(() => {
+    if (setFileImage == null) {
+      return undefined;
+    }
+    (async () => {
+      setBase64(await convertBase64(fileState));
+    })();
+  }, [fileState]);
+
+  const convertBase64 = (file) => {
+    return new Promise((resolve, reject) => {
+      const fileReader = new FileReader();
+      fileReader.readAsDataURL(file);
+
+      fileReader.onload = () => {
+        resolve(fileReader.result);
+      };
+
+      fileReader.onerror = (error) => {
+        reject(error);
+      };
+    });
+  };
+
+  const [isTagInputOpen, setTagInputOpen] = useState(false);
+  const [loading, setLoading] = useState(isTagInputOpen);
+  const [tags, setTags] = useState([]);
+  const [selectedTags, setSelectedTags] = useState([]);
+  const getTags = async (e) => {
+    try {
+      const token = await getAccessTokenSilently();
+      axios.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+      await axios
+        .get("/api/tags")
+        .then((response) => {
+          setLoading(false);
+          setTags(response.data);
+        })
+        .catch((error) => {
+          setLoading(false);
+          toast.error(error.message);
+        });
+    } catch (error) {
+      setLoading(false);
+      toast.error(error.message);
+    }
+  };
+  useEffect(() => {
+    let active = true;
+    if (!isTagInputOpen) {
+      return undefined;
+    }
+    (async () => {
+      if (active) {
+        setLoading(true);
+        await getTags();
+      }
+    })();
+    return () => {
+      active = false;
+    };
+  }, [isTagInputOpen]);
 
   return (
     <Box sx={{ paddingTop: 1 }}>
+      <Divider>
+        <b>Edit Clothes</b>
+      </Divider>
       <Container maxWidth="sm" sx={{ paddingBottom: 5 }}>
-        <img
-          src={"https://images.unsplash.com/photo-1556905055-8f358a7a47b2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80"}
-          alt="selected image"
-          style={{ margin: "auto auto" }}
-        />
-        <FormControl fullWidth margin="normal">
-              <label htmlFor="icon-button-file">
-                <Input
-                  accept="image/*"
-                  id="icon-button-file"
-                  type="file"
-                  onChange={(event) => {
-                    //setFileImage(event.target.files[0]);
-                  }}
-                  sx={{ display: "none" }}
-                />
-                <Button
-                  color="primary"
-                  aria-label="upload picture"
-                  component="span"
-                  variant="outlined"
-                  startIcon={<PhotoCamera />}
-                  fullWidth
-                >
-                  Upload
-                </Button>
-              </label>
+        {base64 && (
+          <img
+            src={base64}
+            alt="selected image"
+            style={{ margin: "auto auto" }}
+          />
+        )}
+        <form onSubmit={postClothings}>
+          <FormControl fullWidth margin="normal">
+            <label htmlFor="icon-button-file">
+              <Input
+                accept="image/*"
+                id="icon-button-file"
+                type="file"
+                onChange={(event) => {
+                  setFileImage(event.target.files[0]);
+                }}
+                sx={{ display: "none" }}
+              />
 
+              <Button
+                color="primary"
+                aria-label="upload picture"
+                component="span"
+                variant="outlined"
+                startIcon={<PhotoCamera />}
+                fullWidth
+              >
+                Upload
+              </Button>
+            </label>
             <TextField
               margin="dense"
               required
@@ -51,17 +195,45 @@ function EditCloth() {
               label="Description"
               variant="outlined"
             />
-            <Stack
-              direction="row"
-              justifyContent="space-around">
-              <Button variant="contained" type="submit">
-                Save
-              </Button>
-              <Button variant="outlined" type="submit">
-                Cancel
-              </Button>
-            </Stack>
-        </FormControl>
+            <Autocomplete
+              multiple
+              open={isTagInputOpen}
+              onOpen={() => {
+                setTagInputOpen(true);
+              }}
+              onClose={() => {
+                setTagInputOpen(false);
+              }}
+              onChange={(event, value) => {
+                setSelectedTags(value);
+              }}
+              isOptionEqualToValue={(option, value) => option.id === value.id}
+              getOptionLabel={(option) => option.title}
+              options={tags}
+              loading={loading}
+              renderInput={(params) => (
+                <TextField
+                  {...params}
+                  label="Tags"
+                  InputProps={{
+                    ...params.InputProps,
+                    endAdornment: (
+                      <>
+                        {loading ? (
+                          <CircularProgress color="inherit" size={20} />
+                        ) : null}
+                        {params.InputProps.endAdornment}
+                      </>
+                    ),
+                  }}
+                />
+              )}
+            />
+            <Button variant="contained" type="submit">
+              Add
+            </Button>
+          </FormControl>
+        </form>
       </Container>
     </Box>
   );
